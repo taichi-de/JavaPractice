ed8c93b129a7dd57049b862c8c32474c4a3c9374
taichi-de <61309248+taichi-de@users.noreply.github.com>
2021-05-05 18:20:19 +0200

    toString

---
+++ src/ToString/CodingIsFun.java
@@ -0,0 +1,12 @@
+package ToString;
+
+public class CodingIsFun {
+
+  public static void main(String[] args) {
+    Student s1 = new Student(1, "Bruce");
+    Student s2 = new Student(2, "Jackie");
+
+    System.out.println(s1.toString());
+    System.out.println(s2.toString());
+  }
+}
---
+++ src/ToString/Student.java
@@ -0,0 +1,12 @@
+package ToString;
+
+public class Student {
+
+  int studentId;
+  String name;
+
+  Student(int studentId, String name){
+    this.studentId = studentId;
+    this.name = name;
+  }
+}
