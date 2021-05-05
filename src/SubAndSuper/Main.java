09346ef4be09b9c86befd1d555ec76bf4bd97e7e
taichi-de <61309248+taichi-de@users.noreply.github.com>
2021-05-05 15:40:38 +0200

    Sub- and Superclass

---
+++ src/SubAndSuper/Main.java
@@ -0,0 +1,9 @@
+package SubAndSuper;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Shoe s = new Shoe("Nike", 10);
+    System.out.println(s.brand);
+  }
+}
---
+++ src/SubAndSuper/Running.java
@@ -0,0 +1,10 @@
+package SubAndSuper;
+
+public class Running extends Shoe{
+  public final double weight;
+
+  Running(double weight, String brand, double size){
+    super(brand, size);
+    this.weight = weight;
+  }
+}
