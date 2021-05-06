1a1edf7b9c8bd7f0a3727a0b80ed87fc63cceadb
taichi-de <61309248+taichi-de@users.noreply.github.com>
2021-05-06 11:04:39 +0200

    Capitalization

---
+++ src/CapProgram.java
@@ -0,0 +1,11 @@
+import java.util.Locale;
+
+public class CapProgram {
+
+  public static void main(String[] args) {
+
+    String s = "happy birthday!";
+    String result = s.substring(0,1).toUpperCase() + s.substring(1);
+    System.out.println(result);
+  }
+}
